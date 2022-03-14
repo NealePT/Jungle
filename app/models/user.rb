b0aa3f4c80a6f11566ad31536a6dba4270d910d2
Neale Taylor <nealeptaylor@gmail.com>
2022-03-14 19:43:32 -0400

    fix email spacing and casing issues on authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,14 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true, length: { minimum: 3 }
   validates :name, presence: true
 
+  before_save do 
+    self.email.downcase! if self.email
+    self.email.strip! if self.email
+  end
+
   def self.authenticate_with_credentials(email, password) 
-    email = email.downcase.strip
-    user = User.find_by_email(email)
+    uemail = email.downcase.strip
+    user = User.find_by_email(uemail)
     user.authenticate(password)
   end
 end
