eeb5caa5f76e6e9d3ae3e8b371c96e8a2d905978
Neale Taylor <nealeptaylor@gmail.com>
2022-03-14 19:20:23 -0400

    add validation methods to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 8 }
-  validates :password_confirmation, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 3 }
+  validates :password_confirmation, presence: true, length: { minimum: 3 }
   validates :name, presence: true
+
+  def self.authenticate_with_credentials(email, password) 
+    email = email.downcase.strip
+    user = User.find_by_email(email)
+    user.authenticate(password)
+  end
 end
