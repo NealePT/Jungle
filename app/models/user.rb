ab49af67d37fc10cafdff2763faecf0e806441ef
Neale Taylor <nealeptaylor@gmail.com>
2022-03-14 19:07:29 -0400

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true, length: { minimum: 8 }
+  validates :name, presence: true
 end
